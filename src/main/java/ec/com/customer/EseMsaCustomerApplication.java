a5bb8c8c6a13c41e87eb2b4ea4f7646606796c4f
Diego Flores <diego.flores.guerra@gmail.com>
2025-04-21 22:34:10 -0500

    Proyecto inicial

---
+++ src/main/java/ec/com/customer/EseMsaCustomerApplication.java
@@ -0,0 +1,13 @@
+package ec.com.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EseMsaCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EseMsaCustomerApplication.class, args);
+	}
+
+}
