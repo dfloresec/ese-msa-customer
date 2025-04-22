528f54b5a0333862adf6b6aa696917dad258e46c
Diego Flores <diego.flores.guerra@gmail.com>
2025-04-22 02:22:07 -0500

    Cambios

---
+++ build/resources/main/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PERSON (id, name, gender, age, identification, address, phone, password, state) 
+VALUES (1, 'Jose Lema', 'M', 18, '1719956859', 'Otavalo sn y principal', '098254785', '1234', true);
+INSERT INTO PERSON (id, name, gender, age, identification, address, phone, password, state) 
+VALUES (2, 'Marianela Montalvo', 'M', 28, '1719956853', 'Amazonas y NNUU', '097548965', '5678', true);
+INSERT INTO PERSON (id, name, gender, age, identification, address, phone, password, state) 
+VALUES (3, 'Juan Osorio', 'M', 38, '1719956852', '13 junio y Equinoccial', '098874587', '1245', true);
