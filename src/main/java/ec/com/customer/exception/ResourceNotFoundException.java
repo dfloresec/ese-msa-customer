d551ced957334ae60b61e5ae38a2dafb65d47f41
Diego Flores <diego.flores.guerra@gmail.com>
2025-04-22 22:46:43 -0500

    Ajustes

--- src/main/java/ec/com/customer/exception/ResourceNotFoundException.java
+++ src/main/java/ec/com/customer/exception/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package ec.com.customer.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -5422432614541673267L;
 
 	public ResourceNotFoundException(String message) {
 		super(message);
