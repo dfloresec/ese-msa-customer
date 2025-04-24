1c8488ea973c0d269f5b722a9b991a45e987ebb5
Diego Flores <diego.flores.guerra@gmail.com>
2025-04-24 14:55:20 -0500

    Validaciones

--- src/main/java/ec/com/customer/handler/ControlExceptionHandler.java
+++ src/main/java/ec/com/customer/handler/ControlExceptionHandler.java
@@ -1,5 +1,9 @@
 package ec.com.customer.handler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import ec.com.customer.exception.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -25,6 +30,16 @@ public class ControlExceptionHandler extends ResponseEntityExceptionHandler {
 	private ResponseEntity<Object> handlerEntityNotFoundException(ResourceNotFoundException ex) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString()).body(ex.getMessage());
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<Object> constraintViolationException(ConstraintViolationException exception) {
+
+		List<Map<String, String>> errorDetails = exception.getConstraintViolations().stream()
+				.map(violation -> Map.of(violation.getPropertyPath().toString(), violation.getMessage()))
+				.collect(Collectors.toList());
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
 	}
 
 }
--- src/main/java/ec/com/customer/services/entities/Person.java
+++ src/main/java/ec/com/customer/services/entities/Person.java
@@ -2,19 +2,26 @@ package ec.com.customer.services.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @MappedSuperclass
 @Data
 public class Person {
 
+	@NotBlank(message = "El campo name es requerido")
 	private String name;
+	@NotBlank(message = "El campo gender es requerido")
 	private String gender;
 	private int age;
 
 	@Column(nullable = false, unique = true)
+	@NotBlank(message = "El campo identification es requerido")
+	@Pattern(regexp = "^\\d{10}$", message = "No cumple el formato")
 	private String identification;
 	private String address;
+	@NotBlank(message = "El campo phone es requerido")
 	private String phone;
 
 }
